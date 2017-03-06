0c5649a85ca1185dce3ed4c038978da7c8baae2c
Per Evensen <per.evensen@gmail.com>
2017-03-05 21:41:52 -0500

    doting

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,6 +24,7 @@ set :deploy_to, '/home/rails/basic'
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, ".env.production"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor/bundle", "public/uploads"
