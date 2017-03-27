5d1b93979e453323c241fec3e9fe43d1b0f2ca6e
Per Evensen <per.evensen@gmail.com>
2017-03-26 22:13:40 -0400

    change to zoho mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "smtp.zoho.com",
     port: 587,
     domain: Rails.application.secrets.domain_name,
     authentication: "plain",
