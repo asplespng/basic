7098fb1a65ab49ae26397874109d1a11499c79cf
Per Evensen <per.evensen@gmail.com>
2017-03-26 22:50:21 -0400

    try ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,9 +70,11 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
     address: "smtp.zoho.com",
-    port: 587,
+    port: 465,
     domain: Rails.application.secrets.domain_name,
     authentication: :login,
+    ssl: true,
+    tls: true,
     enable_starttls_auto: true,
     user_name: Rails.application.secrets.email_provider_username,
     password: Rails.application.secrets.email_provider_password
