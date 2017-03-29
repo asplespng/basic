6001cb950bb7238e4ce4ebff4ffd9b1391040839
Per Evensen <per.evensen@gmail.com>
2017-03-29 15:35:39 -0400

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,12 +71,13 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.zoho.com",
     port: 587,
-    domain: Rails.application.secrets.domain_name,
-    authentication: :login,
+    # domain: Rails.application.secrets.domain_name,
+    authentication: :plain,
     enable_starttls_auto: true,
     user_name: Rails.application.secrets.email_provider_username,
     password: Rails.application.secrets.email_provider_password,
-    ssl: true
+    ssl: true,
+    tls: true
   }
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
