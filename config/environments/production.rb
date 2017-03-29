b2c841b7c2642b1ab3285d09471d75e74218d38a
Per Evensen <per.evensen@gmail.com>
2017-03-29 15:43:16 -0400

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,8 +75,7 @@ Rails.application.configure do
     authentication: :plain,
     enable_starttls_auto: true,
     user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_password,
-    tls: true
+    password: Rails.application.secrets.email_provider_password
   }
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
